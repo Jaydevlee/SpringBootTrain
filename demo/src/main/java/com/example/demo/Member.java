fac0bfa84a72508482c4bba2c72fc285c5871bf5
Jaydevlee <yousick5@gmail.com>
2025-12-02 09:03:38 +0900

    initial commit

---
+++ demo/src/main/java/com/example/demo/Member.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Member {
+    private String id;
+    private String name;
+    private String email;
+    private int age;
+}
