784c192af07bd1830a13b3e756d3a95a68eb2fb8
Jaydevlee <yousick5@gmail.com>
2025-12-04 16:17:21 +0900

    파일이름 변경 및 애플리케이션 일부 수정

--- demoMyBastis/src/main/resources/data.sql
+++ demoMyBastis/src/main/resources/data.sql
@@ -7,8 +7,8 @@ VALUES (MEM_SEQ.NEXTVAL, '공미영', 'MiyeongKong@hanbit.co.kr', 23);
 INSERT INTO MEMBER (id, name, email, age)
 VALUES (MEM_SEQ.NEXTVAL, '김도윤', 'DoyoonKim@hanbit.co.kr', 10);
 COMMIT;
-INSERT INTO ARTICLE(id, title, description, created, updated, member_id) VALUES (art_seq.NEXTVAL, '첫 번째 제목', '첫 번째 본문', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1);
-INSERT INTO ARTICLE(id, title, description, created, updated, member_id) VALUES (art_seq.NEXTVAL, '두 번째 제목', '두 번째 본문', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2);
-INSERT INTO ARTICLE(id, title, description, created, updated, member_id) VALUES (art_seq.NEXTVAL, '세 번째 제목', '세 번째 본문', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3);
-INSERT INTO ARTICLE(id, title, description, created, updated, member_id) VALUES (art_seq.NEXTVAL, '네 번째 제목', '네 번째 본문', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 4);
+INSERT INTO ARTICLE(id, title, description, created, updated, member_id) VALUES (article_seq.NEXTVAL, '첫 번째 제목', '첫 번째 본문', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1);
+INSERT INTO ARTICLE(id, title, description, created, updated, member_id) VALUES (article_seq.NEXTVAL, '두 번째 제목', '두 번째 본문', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2);
+INSERT INTO ARTICLE(id, title, description, created, updated, member_id) VALUES (article_seq.NEXTVAL, '세 번째 제목', '세 번째 본문', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3);
+INSERT INTO ARTICLE(id, title, description, created, updated, member_id) VALUES (article_seq.NEXTVAL, '네 번째 제목', '네 번째 본문', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 4);
 COMMIT;
