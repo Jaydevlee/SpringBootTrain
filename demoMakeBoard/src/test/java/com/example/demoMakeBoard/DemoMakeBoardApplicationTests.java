e0ec574331c430d4cc28a6d099a4fc4012da5536
Jaydevlee <yousick5@gmail.com>
2025-12-09 15:24:09 +0900

    스프링 부트 게시판 만들기

---
+++ demoMakeBoard/src/test/java/com/example/demoMakeBoard/DemoMakeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoMakeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMakeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
