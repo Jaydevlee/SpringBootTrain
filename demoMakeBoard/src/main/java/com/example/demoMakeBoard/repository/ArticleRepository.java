dee2e028f5c64646a44318c2e902a1c32214d24a
Jaydevlee <yousick5@gmail.com>
2025-12-10 08:54:21 +0900

    스프링 부트 게시판 만들기3(게시판리스트 까지)

--- demoMakeBoard/src/main/java/com/example/demoMakeBoard/repository/ArticleRepository.java
+++ demoMakeBoard/src/main/java/com/example/demoMakeBoard/repository/ArticleRepository.java
@@ -1,8 +1,13 @@
 package com.example.demoMakeBoard.repository;
 
+import com.example.demoMakeBoard.model.Article;
 import com.example.demoMakeBoard.model.Member;
 import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
   @Transactional
   void deleteAllByMember(Member member);
 }
